1ec2325c87e03f91eabf0fc4f37b7433083f8d98
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-09-27 14:53:11 -0700

    :microscope: added ShouldMigrateLegacy, issue #8

--- shell/Songhay.Publications.Tests/LegacyMigrationTests.cs
+++ shell/Songhay.Publications.Tests/LegacyMigrationTests.cs
@@ -13,5 +13,74 @@ using Xunit.Abstractions;
 namespace Songhay.Publications.Tests
 {
     public class LegacyMigrationTests
-    { }
+    {
+        public LegacyMigrationTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Theory]
+        [InlineData("../../../../../shell", "../../../json", "index.json")]
+        public void ShouldMigrateLegacy(string shellRoot, string jsonRoot, string indexName)
+        {
+            shellRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, shellRoot);
+            Assert.True(Directory.Exists(shellRoot));
+
+            jsonRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, jsonRoot);
+            Assert.True(Directory.Exists(jsonRoot));
+
+            var shellRootInfo = new DirectoryInfo(shellRoot);
+            var jsonRootInfo = new DirectoryInfo(jsonRoot);
+
+            var jAIndex = JArray.Parse(File.ReadAllText(jsonRootInfo.GetFiles().First(i => i.Name == indexName).FullName));
+
+            shellRootInfo.Parent.GetDirectories("20*").ForEachInEnumerable(i =>
+            {
+                this._testOutputHelper.WriteLine(i.Name);
+                var a = i.Name.Split('-');
+                var year = a.First();
+                var month = a.Last();
+
+                string GetTitleOrSlug(FileInfo info)
+                {
+                    var titleLength = 53; // where did this number come from? 🤷‍
+                    var titleOrSlug = info.Name.Replace(".md", string.Empty);
+                    if (titleOrSlug.Length >= titleLength) titleOrSlug.Substring(0, titleLength);
+
+                    if (titleOrSlug.StartsWith("studio-status-report"))
+                    {
+                        titleOrSlug = $"studio-status-report-{year}-{month}";
+                    }
+
+                    return titleOrSlug;
+                }
+
+                i.GetFiles("*.md").ForEachInEnumerable(j =>
+                {
+                    var titleOrSlug = GetTitleOrSlug(j);
+                    this._testOutputHelper.WriteLine($"looking for `{titleOrSlug}` in index...");
+
+                    // search index by title
+                    var entry = jAIndex.FirstOrDefault(k => k["title"].Value<string>().StartsWith(titleOrSlug));
+                    if (entry != null)
+                    {
+                        this._testOutputHelper.WriteLine($"found {entry["slug"]}");
+                        return;
+                    }
+
+                    // search index by slug
+                    entry = jAIndex.FirstOrDefault(k => k["slug"].Value<string>().StartsWith(titleOrSlug));
+                    if (entry != null)
+                    {
+                        this._testOutputHelper.WriteLine($"found {entry["slug"]}");
+                        return;
+                    }
+
+                    this._testOutputHelper.WriteLine($"WARNING: {titleOrSlug} not found!");
+                });
+            });
+        }
+
+        readonly ITestOutputHelper _testOutputHelper;
+    }
 }
