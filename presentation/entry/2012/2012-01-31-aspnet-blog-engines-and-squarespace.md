5c13f2eba3c1dd45535fe860ad057cedfaeb7675
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-10-02 16:46:49 -0700

    added MarkdownEntryTests to start mass edits :umbrella:, issue #14

--- presentation/entry/2012/2012-01-31-aspnet-blog-engines-and-squarespace.md
+++ presentation/entry/2012/2012-01-31-aspnet-blog-engines-and-squarespace.md
@@ -1,20 +1,30 @@
 ---json
 {
-  "author": "Bryan Wilhite",
-  "content": "Hi wired world. This is my first entry for this new blog—so it makes a bit of sense to write about the ASP.NET-based Blog engines with which I experimented (to start this blog):Orchard CMS is an MVC application so I preferred this. The problem is with my...",
-  "inceptDate": "2012-01-31T23:19:00",
-  "isPublished": true,
-  "slug": "aspnet-blog-engines-and-squarespace",
-  "title": "ASP.NET Blog Engines (and SquareSpace.com)"
+  "documentId": 0,
+  "title": "ASP.NET Blog Engines (and SquareSpace.com)",
+  "documentShortName": "2012-01-31-aspnet-blog-engines-and-squarespace",
+  "fileName": "index.html",
+  "path": "./entry/2012-01-31-asp-net-blog-engines-and-squarespace-com",
+  "date": "2012-02-01T07:19:00.000Z",
+  "modificationDate": "2012-02-01T07:19:00.000Z",
+  "templateId": 0,
+  "segmentId": 0,
+  "isRoot": false,
+  "isActive": true,
+  "sortOrdinal": 0,
+  "clientId": "2012-01-31-aspnet-blog-engines-and-squarespace",
+  "tag": "{\r\n  \"extract\": \"Hi wired world. This is my first entry for this new blog—so it makes a bit of sense to write about the ASP.NET-based Blog engines with which I experimented (to start this blog):Orchard CMS is an MVC application so I preferred this. The problem is with my...\"\r\n}"
 }
 ---
 
+## ASP.NET Blog Engines (and SquareSpace.com)
+
 Hi wired world. This is my first entry for this new blog—so it makes a bit of sense to write about the ASP.NET-based Blog engines with which I experimented (to start this blog):
 
-*   Orchard CMS is an MVC application so I preferred this. The problem is with my hosting service (I use the same one Phil Haack uses—and Phil is using/developing SubText). I got a bunch of permissions errors pointing to the `App_Data` folder and many of the error messages referred to file locations that were clearly on one of the Orchard developer’s machine—not a confidence builder. I also was not comfortable with the over 1900 files that “ships” with Orchard—many of them are `*.cs` files that dynamically compile.
-*   Das Blog is “done” as Scott Hanselman says (which is not to be confused with abandoned). I would have been fine with using it except that Windows Live Writer no longer works with it (or *vice versa*). My hosting service installs Das Blog automatically and without incident.
-*   Since Phil Haack uses SubText I installed SubText (through my hosting provider’s “beta” gallery). It got 500 errors right off the bat. I noticed that the database I assigned to it has no tables in it after the install. Phil and the other SubText guys have nothing to do with this but I’ll pass anyway because it’s not ASP.NET MVC (eventually I’ll give up on demanding ASP.NET MVC for a blog engine).
-*   [Oxite](http://oxite.codeplex.com/) has an ASP.NET MVC engine but as the team confesses, “This isn’t a finished product. It has no install, it has no documentation. It is our code, exactly as we are using it.”
-*   I know that SquareSpace.com is based on Java but I tried it anyway and I had only one problem most SquareSpace.com supporters would care about: when I clicked on a “sub-style” the CSS files seemed to break and my site got stuck with unformatted content (the new style only showed up after moving to another menu item and moving back). Based on the promotional aesthetic of SquareSpace.com, I expected it to be *vastly* superior to the crap I’ve been dealing with since 1998—and when I mean *vast* I mean like alien-from-outer-space-with-superior-technology *vast*. I’m finding that the SquareSpace.com folks are rather ordinary people.
-*   There’s AtomSite: “AtomSite is built with extensible web standards that unleash your social website and blog…” This system runs on <acronym title="Extensible Hypertext Markup Language">XHTML</acronym>—and the latest entry in the AtomSite.net blog is from 4/2010. There is an <acronym title="Extensible Markup Language">XML</acronym> version of HTML 5—so I’m sure there’s something going on there in the future.
-*   Like AtomSite, BlogEngine.NET does not require a database (I prefer this for the feeling of lightness and ease of maintenance). I installed this twice—once through my Host control panel and then again through FTP (because my host did not have an up-to-date version of BlogEngine.NET). BlogEngine.NET installed without any problems (my only challenge had to do with enabling write access to a folder because data is persisted in XML files).
+* Orchard CMS is an MVC application so I preferred this. The problem is with my hosting service (I use the same one Phil Haack uses—and Phil is using/developing SubText). I got a bunch of permissions errors pointing to the `App_Data` folder and many of the error messages referred to file locations that were clearly on one of the Orchard developer’s machine—not a confidence builder. I also was not comfortable with the over 1900 files that “ships” with Orchard—many of them are `*.cs` files that dynamically compile.
+* Das Blog is “done” as Scott Hanselman says (which is not to be confused with abandoned). I would have been fine with using it except that Windows Live Writer no longer works with it (or *vice versa*). My hosting service installs Das Blog automatically and without incident.
+* Since Phil Haack uses SubText I installed SubText (through my hosting provider’s “beta” gallery). It got 500 errors right off the bat. I noticed that the database I assigned to it has no tables in it after the install. Phil and the other SubText guys have nothing to do with this but I’ll pass anyway because it’s not ASP.NET MVC (eventually I’ll give up on demanding ASP.NET MVC for a blog engine).
+* [Oxite](http://oxite.codeplex.com/) has an ASP.NET MVC engine but as the team confesses, “This isn’t a finished product. It has no install, it has no documentation. It is our code, exactly as we are using it.”
+* I know that SquareSpace.com is based on Java but I tried it anyway and I had only one problem most SquareSpace.com supporters would care about: when I clicked on a “sub-style” the CSS files seemed to break and my site got stuck with unformatted content (the new style only showed up after moving to another menu item and moving back). Based on the promotional aesthetic of SquareSpace.com, I expected it to be *vastly* superior to the crap I’ve been dealing with since 1998—and when I mean *vast* I mean like alien-from-outer-space-with-superior-technology *vast*. I’m finding that the SquareSpace.com folks are rather ordinary people.
+* There’s AtomSite: “AtomSite is built with extensible web standards that unleash your social website and blog…” This system runs on <acronym title="Extensible Hypertext Markup Language">XHTML</acronym>—and the latest entry in the AtomSite.net blog is from 4/2010. There is an <acronym title="Extensible Markup Language">XML</acronym> version of HTML 5—so I’m sure there’s something going on there in the future.
+* Like AtomSite, BlogEngine.NET does not require a database (I prefer this for the feeling of lightness and ease of maintenance). I installed this twice—once through my Host control panel and then again through FTP (because my host did not have an up-to-date version of BlogEngine.NET). BlogEngine.NET installed without any problems (my only challenge had to do with enabling write access to a folder because data is persisted in XML files).
---
+++ shell/Songhay.Publications.Tests/MarkdownEntryTests.cs
@@ -0,0 +1,234 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using Songhay.Extensions;
+using Songhay.Publications.Extensions;
+using Songhay.Publications.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Songhay.Publications.Tests
+{
+    public class MarkdownEntryTests
+    {
+        internal static string ProcessParagraph(string p, ITestOutputHelper helper)
+        {
+            MatchEvaluator ReplaceMatchGroupOneWithEmptyString = (Match m) =>
+            {
+                var group = m.Groups[1];
+                return m.Value.Replace(group.Value, string.Empty);
+            };
+
+            MatchEvaluator ReplaceMatchGroupOneWithOneSpace = (Match m) =>
+            {
+                var group = m.Groups[1];
+                return m.Value.Replace(group.Value, " ");
+            };
+
+            helper.WriteLine($"`{p}`");
+
+            p = Regex.Replace(p, @"^(\#+) {2,}", // header with too many spaces
+                m =>
+                {
+                    return $"{m.Groups[1].Value} ";
+                });
+
+            p = Regex.Replace(p, @"^\#+([\r\n]+ +)", // header with line break and any spaces
+                ReplaceMatchGroupOneWithOneSpace);
+
+            p = Regex.Replace(p, @"^\* {2,}", "* "); // unordered list items with too many spaces [first item]
+
+            p = Regex.Replace(p, @"([\r,\n])\* {2,}", // unordered list items with too many spaces
+                m =>
+                {
+                    var group = m.Groups[1];
+                    return $"{group.Value}* ";
+                });
+
+            p = Regex.Replace(p, @"\!\[\]\([^(]+\)", // image without alt text
+                m => m.Value.Replace("![]", "![blog image]"));
+
+            p = Regex.Replace(p, @"\[\!\[[^[]+(\s+)\]\(", // link with image with trailing space(s)
+                ReplaceMatchGroupOneWithEmptyString);
+
+            p = Regex.Replace(p, @"\]\(http[^)]+\)(\s+)\]\(http", // link with image with trailing space(s)
+                ReplaceMatchGroupOneWithEmptyString);
+
+            p = Regex.Replace(p, @"\[\[[^\]]+](\s+)]", // link with brackets with space(s)
+                ReplaceMatchGroupOneWithEmptyString);
+
+            p = Regex.Replace(p, @"\[(\s+)\[[^\]]+]]", // link with brackets with space(s)
+                ReplaceMatchGroupOneWithEmptyString);
+
+            p = Regex.Replace(p, @"\[(\s+)\!\[[^[]+\]", // link with image with leading space(s)
+                m =>
+                {
+                    var group = m.Groups[1];
+                    return m.Value.Remove(1, group.Length);
+                });
+
+            p = Regex.Replace(p, @"\[(\s+)<img", // link with image with leading space(s)
+                m =>
+                {
+                    var group = m.Groups[1];
+                    return m.Value.Remove(1, group.Length);
+                });
+
+            p = Regex.Replace(p, @">(\s+)\]", // link with image (or any HTML element) with trailing space(s)
+                ReplaceMatchGroupOneWithEmptyString);
+
+            p = Regex.Replace(p, @"\[(\s+)\!\[[^\]]+\]\]\([^\)]+(\)\s+\])",
+                // link with Twitter image and handle with leading space(s)
+                m =>
+                {
+                    var group = m.Groups[1];
+                    var s = m.Value.Remove(1, group.Length);
+                    group = m.Groups[2];
+                    return s.Replace(group.Value, ")]");
+                });
+
+            p = p.Replace("\t", "    "); // replace tabs with spaces
+
+            p = Regex.Replace(p, @"\s+$", string.Empty); // trailing spaces after paragraph
+
+            p = Regex.Replace(p, @"[\d]+\.( {2,})", // number mark with too many spaces
+                ReplaceMatchGroupOneWithOneSpace);
+
+            return p.Trim();
+        }
+
+        internal static void ValidateFrontMatter(FileInfo entry)
+        {
+            var markdownEntry = entry.ToMarkdownEntry();
+        }
+
+        public MarkdownEntryTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Theory, InlineData("../../../../../presentation/entry")]
+        public void ShouldEditBlogEntries(string entryRoot)
+        {
+            // arrange
+            entryRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, entryRoot);
+
+            var entryRootInfo = new DirectoryInfo(entryRoot);
+
+            this._testOutputHelper.WriteLine($"Root {entryRootInfo.FullName}...");
+
+            // act
+            entryRootInfo
+                .GetFiles("*.md", SearchOption.AllDirectories)
+                .ForEachInEnumerable(ShouldEditOneBlogEntry);
+        }
+
+        [Theory, InlineData("../../../../../presentation/entry/2012/2012-01-31-aspnet-blog-engines-and-squarespace.md")]
+        public void ShouldEditBlogEntry(string entryPath)
+        {
+            // arrange
+            entryPath = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, entryPath);
+
+            var entryPathInfo = new FileInfo(entryPath);
+
+            this._testOutputHelper.WriteLine($"Root {entryPathInfo.FullName}...");
+
+            // act
+            ShouldEditOneBlogEntry(entryPathInfo);
+        }
+
+        [Theory, InlineData(
+            "../../../../../presentation-drafts",
+            "My Last Reasons to Write")]
+        public void ShouldGenerateEntry(string entryRoot, string title)
+        {
+            // arrange
+            entryRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, entryRoot);
+
+            // act
+            var entry = new MarkdownEntry()
+                .WithNew11tyFrontMatter(title, DateTime.Now, path: "./entry/", tag : null);
+
+            // assert
+            File.WriteAllText($"{entryRoot}/{entry.FrontMatter["clientId"]}.md", entry.ToFinalEdit());
+        }
+
+        void ShouldEditOneBlogEntry(FileInfo entryInfo)
+        {
+            this._testOutputHelper.WriteLine($"Entry {entryInfo.Name}...");
+
+            var finalEdit = entryInfo
+                .ToMarkdownEntry()
+                .WithEdit(i => // frontmatter
+                    {
+                        var content = i.Content;
+
+                        // convert to standard frontmatter:
+                        var title = i.FrontMatter.GetValue<string>("title");
+                        var inceptDate = i.FrontMatter.GetValue<DateTime>("inceptDate");
+                        var path = "./entry/";
+                        var tag = JObject.FromObject(new { extract = i.FrontMatter.GetValue<string>("content") }).ToString();
+                        i.WithNew11tyFrontMatter(title, inceptDate, path, tag);
+
+                        var clientId = entryInfo.Name.Replace(".md", string.Empty);
+                        i.FrontMatter["clientId"] = clientId;
+                        i.FrontMatter["documentShortName"] = clientId;
+
+                        i.Content = string.Concat(i.Content, content);
+                    })
+                .WithEdit(i => // content line breaks
+                    {
+                        i.Content = Regex.Replace(i.Content, // ensure html block line breaks
+                            @"[\r\n]*<\/*(blockquote|div|iframe)[^>]*>[\r\n]*", m =>
+                            {
+                                return string.Concat(
+                                    MarkdownEntry.NewLine,
+                                    MarkdownEntry.NewLine,
+                                    m.Value.Trim(),
+                                    MarkdownEntry.NewLine,
+                                    MarkdownEntry.NewLine);
+                            });
+                        i.Content = Regex.Replace(i.Content, // html block [start] with only one new line
+                            $@"([^\r\n<>]){MarkdownEntry.NewLine}<", m =>
+                            {
+                                var group = m.Groups[1];
+                                return string.Concat(m.Value, MarkdownEntry.NewLine);
+                            });
+                        i.Content = Regex.Replace(i.Content, @"( +)[\r\n]+", m => // line of one or more spaces
+                            {
+                                var group = m.Groups[1];
+                                return m.Value.Replace(group.Value, string.Empty);
+                            });
+                        i.Content = Regex.Replace(i.Content,
+                            $@"{MarkdownEntry.NewLine}{MarkdownEntry.NewLine}({MarkdownEntry.NewLine}+)",
+                            m => // too many line breaks
+                            {
+                                var group = m.Groups[1];
+                                return m.Value.Replace(group.Value, string.Empty);
+                            });
+                    })
+                .WithEdit(i => // paragraphs
+                    {
+                        var paragraphs = i.ToParagraphs();
+                        this._testOutputHelper.WriteLine($"# of paragraphs: {paragraphs.Count()}");
+
+                        i.Content = paragraphs
+                            .Aggregate(
+                                string.Empty,
+                                (a, p) => string.Concat(
+                                    a,
+                                    $"{MarkdownEntry.NewLine}{MarkdownEntry.NewLine}",
+                                    ProcessParagraph(p, this._testOutputHelper)
+                                ));
+                    })
+                .ToFinalEdit();
+
+            this._testOutputHelper.WriteLine($"Writing {entryInfo.FullName}...");
+            File.WriteAllText(entryInfo.FullName, $"{finalEdit}");
+        }
+
+        ITestOutputHelper _testOutputHelper;
+    }
+}
