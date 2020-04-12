e0ac7a9da107d2ee1291916f04620b90383a3407
Bryan Wilhite <rasx@songhaysystem.com>
2020-04-12 15:41:19 -0700

    #25 `ShouldCompressIndex` passes ☔

--- shell/Songhay.Publications.Tests/MarkdownEntryTests.PublicationIndex.cs
+++ shell/Songhay.Publications.Tests/MarkdownEntryTests.PublicationIndex.cs
@@ -1,14 +1,32 @@
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 using Songhay.Extensions;
 using Songhay.Publications.Extensions;
+using Songhay.Tests;
 using Xunit;
 
 namespace Songhay.Publications.Tests
 {
     public partial class MarkdownEntryTests
     {
+        [Theory]
+        [ProjectFileData(typeof(MarkdownEntryTests), "../../../json/index.json")]
+        public void ShouldCompressIndex(FileInfo indexInfo)
+        {
+            using(FileStream fileStream = indexInfo.OpenRead())
+            {
+                using(FileStream compressedFileStream = File.Create(indexInfo.FullName.Replace(".json", ".c.json")))
+                {
+                    using(GZipStream gZipStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                    {
+                        fileStream.CopyTo(gZipStream);
+                    }
+                }
+            }
+        }
+
         [Theory, InlineData(
             "../../../../../presentation/entry", "../../../json", "index.json")]
         public void ShouldGenerateIndex(string entryRoot, string jsonRoot, string indexFileName)
@@ -28,10 +46,10 @@ namespace Songhay.Publications.Tests
                 .Select(jO => JObject.FromObject(new
                 {
                     extract = JObject.Parse(jO.GetValue<string>("tag")).GetValue<string>("extract"),
-                    clientId = jO.GetValue<string>("clientId"),
-                    inceptDate = jO.GetValue<string>("date"),
-                    modificationDate = jO.GetValue<string>("modificationDate"),
-                    title = jO.GetValue<string>("title")
+                        clientId = jO.GetValue<string>("clientId"),
+                        inceptDate = jO.GetValue<string>("date"),
+                        modificationDate = jO.GetValue<string>("modificationDate"),
+                        title = jO.GetValue<string>("title")
                 }))
                 .ToArray();
 
