3af3efa7d894ff31e3dbf69125d1387fdeef1f31
Bryan Wilhite <rasx@songhaysystem.com>
2020-07-24 15:42:49 -0700

    🚀

--- shell/ps1/Upload-StorageAccountContainer.ps1
+++ shell/ps1/Upload-StorageAccountContainer.ps1
@@ -1,7 +1,7 @@
 Push-Location $PSScriptRoot
 
 $accountName = "songhaystorage"
-$containerName = "rasx-context-blog"
+$containerName = "day-path-blog"
 
 $accountKey = "o9yYN8V5A/JqdJAjPpZj9AN3PizChq6Y697kld5681Tqe+GgzB8BqtdF6DaPj3yLfoP0DFW2BD1CxMhiYBTPWg=="
 
